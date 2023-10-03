45159b2532ba35fea754ff43cea214755b6ab4dc
Kareem-Tarek <kareemtarekpk@gmail.com>
2023-10-04 02:52:44 +0300

    some updates

--- resources/views/dashboard/pages/sub-categories/delete.blade.php
+++ resources/views/dashboard/pages/sub-categories/delete.blade.php
@@ -46,7 +46,7 @@
                     <form action="{{ route('subcategories.forceDelete', $subCategory->id) }}" method="post">
                         @csrf
                         @method('DELETE')
-                        <button type="submit" class="btn btn-danger btn-sm font-weight-bold fs-6 mx-1">Permanent Delete</button>
+                        <button type="submit" class="btn btn-danger btn-sm font-weight-bold fs-6 mx-1">Delete from Trash</button>
                     </form>
                 </div>
             </div>
